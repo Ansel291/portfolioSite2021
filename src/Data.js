ac1c6a08d254c5fa1adf9cedfe1265d140c22f72
jason <jason@herman-scheer.com>
2021-12-30 16:38:55 -0800

    added travel blog project

--- src/Data.js
+++ src/Data.js
@@ -3,10 +3,22 @@
 const data = [
     {
         id: 1,
+        title: 'Travel Blog',
+        description: 'A photography travel blog detailing my recent travels in order to document the places I have travelled to recently.',
+        tools: 'React, JavaScript, CSS, Netlify',
+        altSide: 'Travel Blog Side',
+        mainClass: 'travel-blog',
+        sideImage: './images/macbookTravelBlogHero-opt.jpg',
+        projectUrl: 'https://photo-blog-ja-v2.netlify.app/',
+        github: 'https://github.com/Ansel291/photoBlog-2021-12-28',
+        ctaColor: 'blue',
+    },
+    {
+        id: 2,
         title: 'Wanted: Dead or Alive',
         description: 'A timed two-player memory game that involves matching the most famous lawmen/outlaws in the history of the American West.',
         tools: 'CSS3, JavaScript, JQuery, Heroku',
-        altSide: 'Wanted Dead or Alive Sides',
+        altSide: 'Wanted Dead or Alive Side',
         mainClass: 'wanted-dead',
         sideImage: './images/macbook-opt.jpg',
         projectUrl: 'https://ansel291.github.io/Project_1/',
@@ -14,7 +26,7 @@ const data = [
         ctaColor: 'orange',
     },
     {
-        id: 2,
+        id: 3,
         title: 'Cheap Eats of LA',
         description: 'An online blog that allows users to communicate with other users by posting restaurant dishes around Los Angeles and posting reviews.',
         tools: 'Amazon Web Services, Boostrap, Javascript, Ruby on Rails',
@@ -26,7 +38,7 @@ const data = [
         ctaColor: 'blue',
     },
     {
-        id: 3,
+        id: 4,
         title: 'Restful Crawl',
         description: 'Co-Developed Restful Crawl, a group project where our team created an App that allows users to search restaurants, and create a restaurant itinerary to be shared with their friends by replacing text-heavy content with images and maps.',
         tools: 'AJAX, Express, GeoNames, Google API, Google OAuth, Javascript, JSON, MongoDB, Mongoose, Node.js, Yelp API',
@@ -38,7 +50,7 @@ const data = [
         ctaColor: 'orange',
     },
     {
-        id: 4,
+        id: 5,
         title: 'Local City',
         description: 'An App that allows users to search for points of interest on a map and to have each location displayed, and lists an index with a description that matches each location. It is currently in alpha stage of development.',
         tools: 'FourSquare API, Google Maps API, Express, Angular.js, Node.js',
